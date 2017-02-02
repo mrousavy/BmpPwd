49c61c47248cbde643276889e1a193e7b8f0eeaa
mrousavy <marcrousavy@hotmail.com>
2017-02-02 10:16:08 +0100

    Added Classes

---
+++ BmpPwd/BmpPwd.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+
+namespace mrousavy {
+
+    namespace Cryptography {
+        /// <summary>
+        /// En/De-crypt Text to a Bitmap
+        /// </summary>
+        public static class BmpPwd {
+            public static Bitmap Encrypt(string Passphrase, string UnencryptedText) {
+                return BmpPwd.Encrypt(Passphrase, UnencryptedText, new Cipher());
+            }
+
+            public static Bitmap Encrypt(string Passphrase, string UnencryptedText, ICrypt CryptSchema) {
+                Bitmap encrypted;
+
+
+
+                return null;
+            }
+
+
+        }
+
+
+        /// <summary>
+        /// En/De-crypt Text with Passphrases
+        /// </summary>
+        public class Cipher : ICrypt {
+            public string Encrypt(string Passphrase, string UnencryptedText) {
+
+            }
+            public string Decrypt(string Passphrase, string EncryptedText) {
+
+            }
+        }
+    }
+}
---
+++ BmpPwd/ICrypt.cs
@@ -0,0 +1,8 @@
+﻿namespace mrousavy {
+    namespace Cryptography {
+        public interface ICrypt {
+            string Encrypt(string Passphrase, string UnencryptedText);
+            string Decrypt(string Passphrase, string EncryptedText);
+        }
+    }
+}
